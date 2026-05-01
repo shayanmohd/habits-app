6c35578aaef377f17b5327600526c6684f437d0d
shreezyx <87359612+shreezyx@users.noreply.github.com>
2026-05-02 01:15:37 +0530

    Update UI components and versioning for habit tracking app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.mohdshayan.habits"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
